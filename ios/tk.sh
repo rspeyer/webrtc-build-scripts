bc61372266d6967db99ec5e031c6983f5758b259
Richard Speyer <richards@talko.com>
2014-12-23 08:11:20 -0500

    Ability to modify branch/build on command line

--- ios/tk.sh
+++ ios/tk.sh
@@ -3,6 +3,48 @@ BASE_DIR=${HOME}/dev/webrtc-build-scripts
 BRANCH=develop
 BUILD=Release
 
+function usage {
+  echo $1 >&2
+  echo "Usage: $0" >&2
+  echo "        [--branch BRANCH]" >&2
+  echo "        [--build Release|Debug|Profile]" >&2
+}
+
+# 0. Parse arguments
+while [ $# -gt 0 ]
+do
+  case $1 in
+    --branch)
+    BRANCH=$2
+    shift
+    ;;
+    --build)
+    BUILD=$2
+    shift
+    ;;
+    --help)
+    usage "Build and deploy WebRTC binary for iOS into talko_ios project"
+    exit 0
+    ;;
+    -*)
+    usage "Fatal: unknown option: $1"
+    exit 1
+    ;;
+    *)break
+    ;;
+  esac
+  shift
+done
+
+case $# in
+  0)
+  ;;
+  *)
+  usage "Fatal: too many arguments"
+  exit 1
+  ;;
+esac
+
 # 1. Update Code
 pushd ${BASE_DIR}/ios/webrtc/src >/dev/null
 git checkout master
