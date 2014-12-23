659e3906aec0cd215d053504cf7a7f714ecdcd55
Richard Speyer <richards@talko.com>
2014-12-23 15:21:17 -0500

    strip BEFORE libtool (face. palm)

--- ios/build.sh
+++ ios/build.sh
@@ -233,8 +233,8 @@ function build_webrtc_mac() {
 
     if [ "$WEBRTC_RELEASE" = true ] ; then
         exec_ninja "out_ios_x86/Release/"
-        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-mac-x86_64-Release.a" $WEBRTC/src/out_ios_x86/Release/*.a
         exec_strip $WEBRTC/src/out_ios_x86/Release/*.a
+        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-mac-x86_64-Release.a" $WEBRTC/src/out_ios_x86/Release/*.a
     fi
     popd >/dev/null
 }
@@ -263,8 +263,8 @@ function build_apprtc_sim() {
 
     if [ "$WEBRTC_RELEASE" = true ] ; then
         exec_ninja -C "out_ios_x86/Release-iphonesimulator/"
-        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-x86-Release.a" $WEBRTC/src/out_ios_x86/Release-iphonesimulator/*.a
         exec_strip $WEBRTC/src/out_ios_x86/Release-iphonesimulator/*.a
+        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-x86-Release.a" $WEBRTC/src/out_ios_x86/Release-iphonesimulator/*.a
     fi
     popd >/dev/null
 }
@@ -293,8 +293,8 @@ function build_apprtc() {
 
     if [ "$WEBRTC_RELEASE" = true ] ; then
         exec_ninja "out_ios_armeabi_v7a/Release-iphoneos/"
-        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-armeabi_v7a-Release.a" $WEBRTC/src/out_ios_armeabi_v7a/Release-iphoneos/*.a
         exec_strip $WEBRTC/src/out_ios_armeabi_v7a/Release-iphoneos/*.a
+        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-armeabi_v7a-Release.a" $WEBRTC/src/out_ios_armeabi_v7a/Release-iphoneos/*.a
     fi
     popd >/dev/null
 }
@@ -324,8 +324,8 @@ function build_apprtc_arm64() {
 
     if [ "$WEBRTC_RELEASE" = true ] ; then
         exec_ninja "out_ios_arm64_v8a/Release-iphoneos/"
-        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-arm64_v8a-Release.a" $WEBRTC/src/out_ios_arm64_v8a/Release-iphoneos/*.a
         exec_strip $WEBRTC/src/out_ios_arm64_v8a/Release-iphoneos/*.a
+        exec_libtool "$BUILD/libWebRTC-$WEBRTC_REVISION-ios-arm64_v8a-Release.a" $WEBRTC/src/out_ios_arm64_v8a/Release-iphoneos/*.a
     fi
     popd >/dev/null
 }
