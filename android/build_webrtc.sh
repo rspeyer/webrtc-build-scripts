8dc6f3c4e655de1dc9a9ba30c0fa73438e0f5dd3
Richard Speyer <richards@talko.com>
2015-01-07 21:48:48 +0000

    build_webrtc script for Android

---
+++ android/build_webrtc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#  build_webrtc.sh
+#  WebRTC
+#
+#  Created by Richard Speyer on 1/7/15.
+#  Copyright (c) 2015 Talko, Inc. All rights reserved.
+
+PROJECT_DIR=$(dirname $0)
+
+source "$PROJECT_DIR/build.sh"
+build_webrtc
