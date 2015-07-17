c6e7f2f7ad2c32d1b239e94990981f1ed36438f3
Richard Speyer <richards@talko.com>
2015-07-17 14:31:02 -0400

    Massive README cleanup

--- android/provision.sh
+++
@@ -1,7 +0,0 @@
-export VAGRANT_MACHINE=1 
-echo "export VAGRANT_MACHINE=1" >> .bashrc # we set this environment variable so that we put the webrtc code in a shared directory where the host machine can see the files and modify them
-source /vagrant/build.sh
-
-install_dependencies
-
-echo "source /vagrant/build.sh" >> /home/vagrant/.bashrc
